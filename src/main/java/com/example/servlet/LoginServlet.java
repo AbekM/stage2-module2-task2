b8f472aeccfa4bca201f111fd793683074ecc4c3
AbekM <asilbek.makhmudov@outlook.com>
2023-10-09 20:37:24 +0500

    donzo

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,34 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       if (request.getSession().getAttribute("user") == null) {
+           response.sendRedirect(request.getContextPath() + "/login.jsp");
+       } else {
+           response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+       }
+    }
+
+    @Override
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if ("user".equals(login) || "admin".equals(login) && !password.isEmpty()) {
+            request.getSession().setAttribute("user", login);
+            response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+        } else {
+            request.setAttribute("error", "Invalid login credentials");
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
 }
